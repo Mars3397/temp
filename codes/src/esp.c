0846a180b8ff971e9a24ba980b9ce208cb9c286b
Mars3397 <mars3397@gmail.com>
2023-02-26 00:46:41 -0800

    update esp.c

--- codes/src/esp.c
+++ codes/src/esp.c
@@ -15,12 +15,15 @@ void get_ik(int type, uint8_t *key)
     // (Ref. RFC2367 Section 2.3.4 & 2.4 & 3.1.10)
     
     int sock_fd, err;
-    struct sadb_msg msg;
-    struct sadb_sa sa;
-    struct sadb_address src, dst;
-    struct iovec iov;
-    struct msghdr msg_hdr;
-    char buf[4096];
+    struct sadb_msg msg = {
+        .sadb_msg_version = PF_KEY_V2,
+        .sadb_msg_type = SADB_DUMP,
+        .sadb_msg_satype = type,
+        .sadb_msg_len = sizeof(msg) / 8,
+        .sadb_msg_seq = 0,
+        .sadb_msg_pid = getpid(),
+        .sadb_msg_reserved = 0
+    };
 
     // Create a PF_KEY socket
     sock_fd = socket(AF_KEY, SOCK_RAW, PF_KEY_V2);
@@ -29,44 +32,20 @@ void get_ik(int type, uint8_t *key)
         return;
     }
 
-    // Construct a SADB_GET message to retrieve the authentication key
-    memset(&msg, 0, sizeof(msg));
-    msg.sadb_msg_version = PF_KEY_V2;
-    msg.sadb_msg_type = SADB_GET;
-    msg.sadb_msg_satype = type;
-    msg.sadb_msg_len = sizeof(msg) / 8;
-    memset(&sa, 0, sizeof(sa));
-    sa.sadb_sa_len = sizeof(sa) / 8;
-    sa.sadb_sa_exttype = SADB_EXT_SA;
-    memset(&src, 0, sizeof(src));
-    src.sadb_address_len = sizeof(src) / 8;
-    src.sadb_address_exttype = SADB_EXT_ADDRESS_SRC;
-    memset(&dst, 0, sizeof(dst));
-    dst.sadb_address_len = sizeof(dst) / 8;
-    dst.sadb_address_exttype = SADB_EXT_ADDRESS_DST;
-    memset(&iov, 0, sizeof(iov));
-    iov.iov_base = &msg;
-    iov.iov_len = sizeof(msg);
-    memset(&msg_hdr, 0, sizeof(msg_hdr));
-    msg_hdr.msg_name = NULL;
-    msg_hdr.msg_namelen = 0;
-    msg_hdr.msg_iov = &iov;
-    msg_hdr.msg_iovlen = 1;
-
     // Send the SADB_GET message to the kernel
-    err = sendmsg(sock_fd, &msg_hdr, 0);
-    if (err < 0) {
-        perror("sendmsg");
+    if (write(sock_fd, &msg, sizeof(msg)) < 0) {
+        perror("write");
         close(sock_fd);
         return;
     }
 
     // Receive the SADB_GET message response from the kernel
-    iov.iov_base = buf;
-    iov.iov_len = sizeof(buf);
-    err = recvmsg(sock_fd, &msg_hdr, 0);
-    if (err < 0) {
-        perror("recvmsg");
+    char buf[1024];
+    memset(&buf, 0, 1024);
+    ssize_t len = recv(sock_fd, &buf, 1024, 0);
+    printf("%s", buf);
+    if (len < 0) {
+        perror("read");
         close(sock_fd);
         return;
     }
@@ -79,7 +58,7 @@ void get_ik(int type, uint8_t *key)
             memcpy(key, (char *)key_ext + sizeof(struct sadb_key), key_ext->sadb_key_bits / 8);
             break;
         }
-        ext = (struct sadb_ext *)((char *)ext + PFKEY_ALIGN8LEN(ext->sadb_ext_len) * 8);
+        ext = (struct sadb_ext *)((char *)ext + align8(ext->sadb_ext_len) * 8);
     }
 
     close(sock_fd);
